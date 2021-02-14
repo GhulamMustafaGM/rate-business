d89968325b419efb1364c431579eda1be84ca213
Ghulam Mustafa <mustafaji@gmail.com>
2021-02-14 02:54:59 +0100

    added project files

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
+
+
+mongoose.connect('mongodb://localhost/rate-business');
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
@@ -14,6 +19,14 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'Thisismytestkey',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
 
 app.get('/', function(req, res, next) {
     res.render('index');
